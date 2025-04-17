c344474cf8a8ed03255b472f222da3078837d054
ArmNewbyDev <92511077+ArmNewbyDev@users.noreply.github.com>
2025-04-18 00:54:37 +0700

    Add Squash
    - Try Add Idle Fox
    - Add Squash
    - Fix Trash Pick to Pick Interact OverAll

--- Assets/Scripts/ImportanceScripts/ClickInteract.cs
+++ Assets/Scripts/ImportanceScripts/ClickInteract.cs
@@ -1,26 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Data;
 using UnityEngine;
 
-public class PickTrash : MonoBehaviour
+public class ClickInteract : MonoBehaviour
 {
     RaycastHit hitInfo;
+    //IInteract interact;
     private void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hitInfo))
         {
-            if (Input.GetMouseButtonDown(0) && hitInfo.collider.CompareTag("Trash"))
+            if (Input.GetMouseButtonDown(0))
             {
                 Debug.Log("Hit" + hitInfo.collider.name);
-                hitInfo.collider.GetComponent<Trash>().Interact();
+
+                if (hitInfo.collider.TryGetComponent(out IInteract _interact))
+                {
+                    _interact.Interact();
+                }
+
             }
 
-            
+
         }
 
     }
-
 }
