6fb3cdbabfc1b097762c0038484340ff9664a89e
TanawatJira <97783368+Delphiros@users.noreply.github.com>
2025-04-19 02:57:58 +0700

    Rework TextBox

---
+++ Assets/Scripts/Text/CharacterChatBox.cs
@@ -0,0 +1,146 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+
+
+public class CharacterChatBox : MonoBehaviour
+{
+    [SerializeField] private GameObject chatboxPrefabs;
+    public float displayDuration = 5f;
+
+    [SerializeField]private Vector3 chatboxOffset = new Vector3(0f, 2f, 0f);
+    public float LookAtCameraSpeed = 5f;
+
+    private Queue<string> messageQueue = new Queue<string>();
+    private GameObject currentChatbox;
+    private TMP_Text chatboxText;
+
+    private Camera mainCamera;
+    public bool isShowingMessage;
+    private Coroutine showMessageCoroutine;
+    private bool skipToNext = false;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    public void Say(string message)
+    {
+        messageQueue.Enqueue(message);
+        if (!isShowingMessage && messageQueue.Count > 0)
+        {
+            showMessageCoroutine = StartCoroutine(ShowMessageSequence());
+        }
+    }
+
+    private void Update()
+    {
+        if (isShowingMessage && Input.GetMouseButtonDown(0))
+        {
+            skipToNext = true;
+        }
+    }
+
+
+    public void GoToNextMessage()
+    {
+        if (isShowingMessage && messageQueue.Count > 0)
+        {
+            StopCoroutine(ShowMessageSequence());
+            showMessageCoroutine = StartCoroutine(ShowMessageSequence());
+            
+        }
+        else if (isShowingMessage && messageQueue.Count == 0 && currentChatbox != null)
+        {
+            isShowingMessage = false;
+            Destroy(currentChatbox);
+            currentChatbox = null;
+            chatboxText = null;
+        }
+    }
+
+    IEnumerator ShowMessageSequence()
+    {
+        isShowingMessage = true;
+
+        if (currentChatbox == null && chatboxPrefabs != null)
+        {
+            currentChatbox = Instantiate(chatboxPrefabs, transform.position + chatboxOffset, quaternion.identity);
+            chatboxText = currentChatbox.GetComponentInChildren<TMP_Text>();
+            if (chatboxText == null)
+            {
+                Debug.Log("ShowMessageSequence chatboxtext NULL");
+            }
+        }
+        
+
+        while (messageQueue.Count > 0 && chatboxText != null)
+        {
+            string messageShow = messageQueue.Dequeue();
+            chatboxText.text = messageShow;
+            skipToNext = false;
+            
+            float timer = 0f;
+
+            while (timer < displayDuration && !skipToNext)
+            {
+                timer += Time.deltaTime;
+                yield return null;
+            }
+
+            skipToNext = false;
+            /*yield return ShowSingleMessage(messageShow);
+            yield return new WaitForSeconds(displayDuration);
+            Destroy(currentChatbox);
+            currentChatbox = null;*/
+        }
+        
+        isShowingMessage = false;
+        if (currentChatbox != null)
+        {
+            Destroy(currentChatbox);
+            currentChatbox = null;
+            chatboxText = null;
+        }
+
+    }
+
+    /*IEnumerator ShowSingleMessage(string message)
+    {
+        if (chatboxPrefabs != null)
+        {
+            currentChatbox = Instantiate(chatboxPrefabs, this.transform.position + chatboxOffset, Quaternion.identity);
+            chatboxText = currentChatbox.GetComponentInChildren<TMP_Text>();
+            
+            if (chatboxText != null)
+            {
+                chatboxText.text = message;
+            }
+            else
+            {
+                Debug.Log("ChatboxText NUll");
+            }
+
+            yield return null;
+        }
+        else
+        {
+            Debug.Log("Prefabs Chatbox null");
+        }
+    }*/
+
+    private void LateUpdate()
+    {
+        if (currentChatbox != null && mainCamera != null)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(currentChatbox.transform.position - mainCamera.transform.position);
+            currentChatbox.transform.rotation = Quaternion.Slerp(currentChatbox.transform.rotation, targetRotation,
+                Time.deltaTime * LookAtCameraSpeed);
+        }
+    }
+}
--- Assets/Scripts/Text/CharacterShowText.cs
+++ Assets/Scripts/Text/CharacterShowText.cs
@@ -2,23 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterShowText : MonoBehaviour
+public class CharacterShowText : MonoBehaviour, IInteract
 {
-    private Chatbox chatbox;
-    private bool isShow = true; //condition something to show
-    [SerializeField] private string message;
+    private CharacterChatBox chatbox;
+    [SerializeField] private string[] message;
+    public int messageIndex;
     
     void Start()
     {
-        chatbox = GetComponent<Chatbox>();
+        chatbox = GetComponent<CharacterChatBox>();
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    public void Interact()
     {
-        if (isShow && Input.GetKey(KeyCode.D))
+        Debug.Log("interact");
+        if (messageIndex < message.Length)
         {
-            chatbox.ShowMessage(message);
+            chatbox.Say(message[messageIndex]);
+            messageIndex++;
+        }
+        else
+        {
+            Debug.Log("index reset");
+            messageIndex = 0;
         }
     }
 }
