0bc37855718464f8e7d67b4345c25b7cb4bdcd67
ArmNewbyDev <92511077+ArmNewbyDev@users.noreply.github.com>
2025-04-19 17:36:10 +0700

    Add Parrallax, Forest to Scene
    - Add Animation Trash and Prefab
    - Add Parallax
    - Add Forest to Scene Game

---
+++ Assets/Scripts/ArtChangeToColorOrBlack/ChangeColorSprite.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorSprite : MonoBehaviour
+{
+    private SpriteRenderer _renderer;
+    [SerializeField] private Sprite spriteColor;
+    [SerializeField] private Sprite spriteUnColor;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+        ChangeColor(false);
+    }
+
+    private void Update()
+    {
+        if (GameManager.Instance.IsChangeColor)
+        {
+            ChangeColor(true);
+        }
+        else
+        {
+            ChangeColor(false);
+        }
+    }
+
+    public void ChangeColor(bool IsColor)
+    {
+        if (IsColor)
+        {
+            _renderer.sprite = spriteColor;
+        }
+        else
+        {
+            _renderer.sprite = spriteUnColor;
+        }
+
+    }
+
+}
