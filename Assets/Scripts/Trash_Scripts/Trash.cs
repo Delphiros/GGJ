ed168beddb3efec191e64a25049c51340fb3f648
TanawatJira <97783368+Delphiros@users.noreply.github.com>
2025-04-20 22:47:39 +0700

    Add textbox to fox

--- Assets/Scripts/Trash_Scripts/Trash.cs
+++ Assets/Scripts/Trash_Scripts/Trash.cs
@@ -20,7 +20,7 @@ public class Trash : MonoBehaviour, IInteract
         GameManager.Instance.GetTrash(SoundName.Wink);
         // Animation Before Delete
 
-        Destroy(gameObject, 0.5f);
+        Destroy(gameObject, 0.1f);
     }
 
     public void PlayHint()
