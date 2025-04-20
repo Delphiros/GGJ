e1b2832ec7d79cd88a3fb67ab826e15418dbbeee
Looktorzz <kulisutt.j@gmail.com>
2025-04-20 21:55:49 +0700

    Update UI

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -9,43 +9,18 @@ public class UIManager : MonoBehaviour
     private BasePanel _titlePanel;
 
     [SerializeField]
-    private BasePanel _optionPanel;
-
-    [SerializeField]
-    private BasePanel _gameplayPanel;
-
-    [SerializeField]
     private Button _startButton;
-
-    [SerializeField]
-    private Button _optionButton;
-
-    [SerializeField]
-    private Button _closeOptionButton;
-
-    [SerializeField]
-    private Scrollbar _bgmScrollbar;
-
-    [SerializeField]
-    private Scrollbar _sfxScrollbar;
 
     public static UIManager Instance { get; private set; }
     public static bool HasInstance => Instance != null;
 
     public event Action WhenStartedGame;
-    public event Action<float> WhenBGMChanged;
-    public event Action<float> WhenSFXChanged;
-    
     private bool _isInitialized;
-    
     
     
     private void Start()
     {
-        // for test
         Init();
-        
-        
     }
 
     public void Init()
@@ -58,39 +33,5 @@ public class UIManager : MonoBehaviour
 
         Instance = this;
         _titlePanel.OpenPanel();
-        _optionPanel.ClosePanel();
-        _gameplayPanel.ClosePanel();
-
-        _bgmScrollbar.onValueChanged.AddListener(OnBGMChanged);
-        _sfxScrollbar.onValueChanged.AddListener(OnSFXChanged);
-
-        _startButton.onClick.AddListener(OnStartGame);
-        _optionButton.onClick.AddListener(_optionPanel.OpenPanel);
-        _closeOptionButton.onClick.AddListener(_optionPanel.ClosePanel);
-        
-        _isInitialized = true;
-    }
-
-    public void OnBGMChanged(float value)
-    {
-        WhenBGMChanged?.Invoke(value);
-        Debug.Log($"BGM : {value}");
-    }
-
-    public void OnSFXChanged(float value)
-    {
-        WhenSFXChanged?.Invoke(value);
-        Debug.Log($"SFX : {value}");
-    }
-
-    private void OnStartGame()
-    {
-        WhenStartedGame?.Invoke();
-
-        _titlePanel.ClosePanel();
-        _optionPanel.ClosePanel();
-        _gameplayPanel.OpenPanel();
-        
-        
     }
 }
